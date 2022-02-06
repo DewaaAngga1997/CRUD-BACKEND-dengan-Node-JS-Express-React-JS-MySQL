3bcaad09f21e2df29c8c3f756d3537ca3cd8994a
dewaangga <devaangga53@gmail.com>
2022-02-06 09:27:00 +0800

    menampilkan data pruduct ke halaman product

--- controllers/Products.js
+++ controllers/Products.js
@@ -1,4 +1,12 @@
 //halaman product yang di tampilkan di web
-export const getAllPoducts = (req, res) => {
-  res.send('Wellcome');
+import Product from '../models/productModel.js';
+
+export const getAllPoducts = async (req, res) => {
+  //menampilkan product ke web
+  try {
+    const products = await Product.findAll();
+    res.json(products);
+  } catch (error) {
+    res.json({ massage: error.massage });
+  }
 };
