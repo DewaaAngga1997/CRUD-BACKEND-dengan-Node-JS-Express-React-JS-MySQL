7f8fae4e7e8a32950709444a4771c6d363ae2d34
dewaangga <devaangga53@gmail.com>
2022-02-06 11:29:29 +0800

    function update done

--- controllers/Products.js
+++ controllers/Products.js
@@ -10,12 +10,40 @@ export const getAllPoducts = async (req, res) => {
     res.json({ massage: error.massage });
   }
 };
-
+//create
 export const createPoduct = async (req, res) => {
   //menampilkan product ke web
   try {
     await Product.create(req.body);
     res.json({ massage: 'Product Created' });
+  } catch (error) {
+    res.json({ massage: error.massage });
+  }
+};
+//select
+export const getPoductById = async (req, res) => {
+  //menampilkan product ke web
+  try {
+    const product = await Product.findAll({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(product[0]);
+  } catch (error) {
+    res.json({ massage: error.massage });
+  }
+};
+//update
+export const updateProduct = async (req, res) => {
+  //menampilkan product ke web
+  try {
+    await Product.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json({ massage: 'Product Update' });
   } catch (error) {
     res.json({ massage: error.massage });
   }
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 //halaman ini mengatur perpindahan halaman
 import Express from 'express';
-import { getAllPoducts, createPoduct } from '../controllers/Products.js';
+import { getAllPoducts, createPoduct, getPoductById, updateProduct } from '../controllers/Products.js';
 
 const router = Express.Router();
 
 router.get('/', getAllPoducts);
+router.get('/:id', getPoductById);
 router.post('/', createPoduct);
+router.patch('/:id', updateProduct);
 
 export default router;
